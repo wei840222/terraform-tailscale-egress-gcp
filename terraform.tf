be33dfd28a35b5126e443266a726a1bd50d40203
wei840222 <wei840222@gmail.com>
2022-12-11 21:26:12 +0800

    using minio for tfstate

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,16 @@ terraform {
       version = ">= 4.38.0"
     }
   }
+  backend "s3" {
+    endpoint                    = "https://api-minio.weii.dev"
+    region                      = "main"
+    bucket                      = "terraform-state"
+    key                         = "terraform-tailscale-egress-gcp.tfstate"
+    force_path_style            = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+  }
 }
 
 provider "google" {
